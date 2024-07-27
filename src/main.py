d7deb35c814da2ebf151417d55c014857d6548f6
SvAS <sv4s.night@gmail.com>
2024-07-27 16:51:35 +0300

    create project

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from src.utils import receiving_data
+
+
+def main():
+    """Главная функция"""
+    dframe = receiving_data()
+    return dframe
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+""" Код, который повторяется """
+
+import pandas as pd
+
+
+def receiving_data():
+    """Функция получения данных для программы"""
+
+    data_frame = pd.read_excel("../data/operations.xlsx")
+    return print(data_frame)
